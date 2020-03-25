ade624eb70012a05fe3ad928ef4a9d2ec8b3fd60
Horace Gtz <horackone@gmail.com>
2020-03-24 23:57:56 -0600

    Agregando archivos de sitio web a mi repositorio

---
+++ pruebaJS.js
@@ -0,0 +1,2 @@
+var nuevaVar = "Texto";
+nuevaVar
---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+/*eslint-env browser*/
+
+
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+  var miSrc = miImage.getAttribute('src');
+  if (miSrc === '/images/mozilla.png') {
+    miImage.setAttribute('src', '/images/mozilla2.png');
+  } else {
+    miImage.setAttribute('src', '/images/mozilla.png');
+  }
+}
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es fresco1, ' + miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    var nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es fresco2, ' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
